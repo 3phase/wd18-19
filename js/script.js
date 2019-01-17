610344000e0ed4bd1e8ecf262adc4f7a6b3a07b3
3phase <p.bozhinov@keydev.eu>
2019-01-17 12:36:52 +0200

    Add files that we did in class w/ V-class

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    function writeToTable(dataToWrite) {
+        $("article#" + dataToWrite.subjId + " table tbody").append(
+            '<tr>' +
+            '<td>' + dataToWrite.classId + '</td>\n' +
+            '<td>' + dataToWrite.num + '</td>\n' +
+            '<td>' + dataToWrite.name + '</td>\n' +
+            '<td>' + dataToWrite.gradeOne + '</td>\n' +
+            '<td>' + dataToWrite.gradeTwo + '</td>\n' +
+            '</tr>'
+        );
+    } 
+
+    $("#student-data").submit(function(e) {
+        e.preventDefault();
+
+        var dataObj = {
+            'classId': $("select[name='class']").val(),
+            'subjId': $("select[name='subject']").val(),
+            'num': $("input[name='number']").val(),
+            'name': $("input[name='name']").val(),
+            'gradeOne': $("select[name='grade-one']").val(),
+            'gradeTwo': $("select[name='grade-two']").val()
+        };
+        
+        writeToTable(dataObj);
+
+    });
+
+});
