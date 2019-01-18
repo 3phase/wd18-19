eb05601f3e26910f5df4f1d0a18f8d117a68fd12
3phase <p.bozhinov@keydev.eu>
2019-01-18 14:02:05 +0200

    Add code written w/ class B

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    function writeToTable(objToWrite) {
+        $("article#" + objToWrite.subjId + " table tbody").append(
+            "<tr>" + 
+            "<td>" + objToWrite.classId + "</td>" + 
+            "<td>" + objToWrite.num + "</td>" + 
+            "<td>" + objToWrite.name + "</td>" + 
+            "<td>" + objToWrite.gradeOne + "</td>" +     
+            "<td>" + objToWrite.gradeTwo + "</td>" + 
+            "</tr>",
+        );
+    }    
+    
+    $("#student-data").submit(function(e) {
+        e.preventDefault();
+
+        var dataObj = {
+            'classId': $("select[name='class']").val(),
+            'subjId': $("select[name='subject']").val(),
+            'num': $("input[name='number']").val(),
+            'name': $("input[name='name']").val(),
+            'gradeOne': $("select[name='grade-one']").val(),
+            'gradeTwo': $("select[name='grade-two']").val()
+        };
+
+        writeToTable(dataObj);
+
+    });
+    
+});
