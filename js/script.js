bd04157ad1e7ca2acea531cfa7c81a484a09823b
3phase <p.bozhinov@keydev.eu>
2019-04-24 18:20:40 +0300

    Keeps repo up to date

--- js/script.js
+++ js/script.js
@@ -9,36 +9,6 @@ $(document).ready(function() {
         'math': [],
         'wd': []
     };
-	
-    function addDataToStudentInfo(dataObj) {
-        studentInfo[dataObj.subjId].push(dataObj);
-    }
-
-    function isDataValid(data) {
-        $("#student-data").validate({
-            rules: {
-                number: {
-                    required: true,
-                    number: true
-                },
-                name: {
-                    required: true,
-                    lettersonly: true
-                }
-            },
-            messages: {
-                number: 'Моля, въведете стойност за номер, която да е целочислена.',
-                name: 'Моля, въведете стойност за името, състояща се само от букви',
-            }
-        });
-        return $("#student-data").valid();
-    }
-
-    var studentInfo = {
-        'bel': [],
-        'math': [],
-        'wd': []
-    };
 
     function addDataToDataStructure(data) {
         studentInfo[data.subjId].push(data);
@@ -72,8 +42,9 @@ $(document).ready(function() {
             "</tr>",
         );
     } 
+    
     $("#student-data").submit(function(e) {
-        e.preventDefault();
+        e.preventDefault();  
 
         var dataObj = {
             'classId': $("select[name='class']").val(),
@@ -91,5 +62,5 @@ $(document).ready(function() {
             alert('Въведените данни са грешни.');
         }
     });
-    
+
 });
